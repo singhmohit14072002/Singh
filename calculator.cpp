eac8a2b19d6b149b3d1e2685ec8df0ee4ce4f793
iamsinghjp <singhmohit14072002@gmail.com>
2021-03-30 17:20:08 +0530

    calculator made it

---
+++ calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+float n1,n2;
+cout<<"Input 2 numbers: ";
+cin>>n1>>n2;
+
+char op;
+cout<<"input an operator";
+cin>>op;
+
+switch (op){
+
+    case '+':
+    cout<<n1+n2<<endl;
+    break;
+
+    case '-':
+    cout<<n1-n2<<endl;
+    break;
+
+     case '*':
+    cout<<n1*n2<<endl;
+    break;
+    
+     case '/':
+    cout<<n1/n2<<endl;
+    break;
+
+    defult:
+    cout<<"Enter anthor operator"<<endl;
+
+}
+
+
+
+
+
+    return 0;
+}
+
+
+
